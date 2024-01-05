7227492b7bb62e509ca103d814d94a00d96ce4d3
joergi <1439809+joergi@users.noreply.github.com>
2024-01-05 17:09:28 +0100

    [step 0] - setup for starting

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dependencies
+## Step 0
+(scroll down for english explanation)
+Das ist die Ausgangslage - ein einfaches 
---
+++ demo1/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.21"
+	kotlin("plugin.spring") version "1.9.21"
+}
+
+group = "io.joergi"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(libs.springBootStarterWeb)
+	implementation(libs.jacksonKotlinModule)
+	implementation(libs.kotlinReflect)
+	compileOnly(libs.lombok)
+	annotationProcessor(libs.lombok)
+	testImplementation(libs.springBootStarterTest)
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "21"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
